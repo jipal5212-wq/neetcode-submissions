a58967bbced4afac8c3bc2b5c7585598dd440dc3
ashish <jipal5212@gmail.com>
2026-07-29 14:37:34 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+       int n= nums.size();
+       unordered_map<int,int>freq;
+       for(int i=0;i<n;i++){
+        freq[nums[i]]++;
+        if(freq[nums[i]]>1){
+            return true;
+        }
+       } 
+   return false; }
+};
